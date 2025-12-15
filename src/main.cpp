087518e0695052a0ad958dbef16c1076c766d553
Amit <amit_1497@AMITs-MacBook-Air.local>
2025-12-15 14:19:47 +0000

    StringArray -  String Operations

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@ int main() {
     // 1. CREATE AND WRITE
     // ==========================================
     std::cout << "1. Creating data..." << std::endl;
-    arrow::Int64Builder builder;
-    builder.Append(1);
-    builder.Append(2);
+    arrow::StringBuilder builder;
+    builder.Append("Alice");
+    builder.Append("Bob");
     builder.AppendNull();
-    builder.Append(4);
+    builder.Append("Dave");
     
     std::shared_ptr<arrow::Array> array;
     builder.Finish(&array);
 
-    auto schema = arrow::schema({arrow::field("numbers", arrow::int64())});
+    auto schema = arrow::schema({arrow::field("Strings", arrow::utf8())});
     auto table = arrow::Table::Make(schema, {array});
 
     std::shared_ptr<arrow::io::FileOutputStream> outfile;
@@ -55,7 +55,6 @@ int main() {
     std::cout << "3. Computing Sum (Manually)..." << std::endl;
     
     auto column = read_table->column(0)->Slice(2,2);
-    int64_t sum = 0;
     int64_t count = 0;
 
     // A column in a Table is a "ChunkedArray" (it might be split into pieces).
@@ -64,18 +63,17 @@ int main() {
         
         // We must cast the generic Array to the specific type we expect (Int64Array)
         // This gives us access to Value() and IsValid()
-        auto int64_chunk = std::static_pointer_cast<arrow::Int64Array>(chunk);
-        
-        for (int64_t i = 0; i < int64_chunk->length(); ++i) {
+        auto string_chunk = std::static_pointer_cast<arrow::StringArray>(chunk);
+        std::cout << "String Chunk Value:" << string_chunk << "\n";
+        for (int64_t i = 0; i < string_chunk->length(); ++i) {
             // CRITICAL: Always check if the value is valid (not null) before using it!
-            if (int64_chunk->IsValid(i)) {
-                sum += int64_chunk->Value(i);
+            if (string_chunk->IsValid(i)) {
+                std::cout << string_chunk->GetString(i) << "\n";
                 count++;
             }
         }
     }
 
-    std::cout << "   Success! Sum: " << sum << std::endl;
     std::cout << "   (Calculated from " << count << " non-null values)" << std::endl;
 
     return 0;
